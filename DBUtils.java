1234dbda773546171d77ac51e584bd8a53be4f02
syampavan6806 <139321405+syampavan6806@users.noreply.github.com>
2024-10-09 18:58:21 +0530

    Update DBUtils.java

--- DBUtils.java
+++ DBUtils.java
@@ -2,4 +2,5 @@ This line got updated in this file........
 This file contains database info........
 This line is upadted in master branch
 
-This line is git pull
+This line is for git pull demo
+This line is for git fetch demo
